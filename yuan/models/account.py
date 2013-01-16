e234fe59f9de4ff6b22524a7b9734aaf0943b6a7
Hsiaoming Yang <lepture@me.com>
2013-01-16 16:06:32 +0800

    mess commit on front end design

--- yuan/models/account.py
+++ yuan/models/account.py
@@ -63,9 +63,9 @@ class Account(db.Model, SessionMixin):
     def __repr__(self):
         return '<Account: %s>' % self
 
-    def get_avatar(self, size=48):
-        if self.avatar:
-            return self.avatar
+    @cached_property
+    def avatar(self):
+        size = 48
         md5email = hashlib.md5(self.email).hexdigest()
         query = "%s?s=%s%s" % (md5email, size, db.app.config['GRAVATAR_EXTRA'])
         return db.app.config['GRAVATAR_BASE_URL'] + query
