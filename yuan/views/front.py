8a2b1779d70c40b4322ece329578e4d00a16f32d
qiaohua <shengyan1985@gmail.com>
2014-03-11 18:57:06 +0800

    U: style

--- yuan/views/front.py
+++ yuan/views/front.py
@@ -85,7 +85,7 @@ def home():
     for obj in latest_publish_obj:
         obj["pretty_date"] = pretty_date(datetime.strptime(obj["update_at"], '%Y-%m-%dT%H:%M:%SZ'))
         account_query = Account.query.filter_by(name=obj["publisher"])
-        #obj["account"] = account_query and account_query.first() or ""
+        obj["account"] = account_query and account_query.first() or ""
 
     def _get_max(filename):
         fpath = os.path.join(
