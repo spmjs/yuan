5510a22adef35c865f66cf2e75c535a5a7cd2212
qiaohua <shengyan1985@gmail.com>
2014-03-11 19:03:37 +0800

    U: debug

--- yuan/views/front.py
+++ yuan/views/front.py
@@ -151,7 +151,7 @@ def home():
         "total_user_count": Account.query.count(),
         "latest_publisher": latest_publish_obj[0:current_app.config["LIST_MAX_COUNT"]/2],
         "most_depended_upon": _get_unique_max("depend.json"),
-        "top_submittors": []
+        "top_submittors": _get_max("publishers.json")
     }
     return render_template('home.html', **dct)
 
